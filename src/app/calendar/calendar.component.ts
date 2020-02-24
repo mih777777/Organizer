a6f4b9fecefe7fc1a84cde815bd50b2edf99dbaf
mih777 <mih777777@yandex.ru>
2020-02-24 20:07:24 +0300

    30 мин

---
+++ src/app/calendar/calendar.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core'
+import * as moment from 'moment'
+import { DateService } from '../shared/date.service'
+
+interface Day {
+  value: moment.Moment
+  active: boolean
+  disabled: boolean
+  selected: boolean
+}
+
+interface Week {
+  days: Day[]
+}
+
+@Component({
+  selector: 'app-calendar',
+  templateUrl: './calendar.component.html',
+  styleUrls: ['./calendar.component.scss']
+})
+export class CalendarComponent implements OnInit {
+
+  calendar: Week[]
+
+  constructor(private dateService: DateService) { }
+
+  ngOnInit(): void {
+    this.dateService.date.subscribe(this.generate.bind(this))
+  }
+
+  generate(now: moment.Moment) {
+    console.log(now.format())
+  }
+
+}
